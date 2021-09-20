bc667de5064752795987b88db5427f0975556491
Victoria_Scherba <victoria.sch@voicenter.com>
2021-09-20 14:22:40 +0300

    Create telegram bot

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf('2025056057:AAHEG4Qzyv9QJrYd6s7tGcH1hBHsq5obPYM')
+require('dotenv').config();
 
-let qwertyArray = {'q':'й','w':'ц','e':'у','r':'к','t':'е','y':'н','u':'г','i':'ш','o':'щ','p':'з','[':'х'
-                    ,']':'ї','a':'ф','s':'і','d':'в','f':'а','g':'п','h':'р','j':'о','k':'л'
-                    ,'l':'д',';':'ж',"'":"є",'z':'я','x':'ч','c':'с','v':'м','b':'и','n':'т','m':'ь',',':'б','.':'ю'};
+const { Telegraf } = require('telegraf');
 
-let test = Object.entries(qwertyArray);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const qwertyArray = {
+  q: 'й',
+  w: 'ц',
+  e: 'у',
+  r: 'к',
+  t: 'е',
+  y: 'н',
+  u: 'г',
+  i: 'ш',
+  o: 'щ',
+  p: 'з',
+  '[': 'х',
+  ']': 'ї',
+  a: 'ф',
+  s: 'і',
+  d: 'в',
+  f: 'а',
+  g: 'п',
+  h: 'р',
+  j: 'о',
+  k: 'л',
+  l: 'д',
+  ';': 'ж',
+  "'": 'є',
+  z: 'я',
+  x: 'ч',
+  c: 'с',
+  v: 'м',
+  b: 'и',
+  n: 'т',
+  m: 'ь',
+  ',': 'б',
+  '.': 'ю',
+};
+
+const test = Object.entries(qwertyArray);
 
 bot.on('text', (ctx) => {
-    let userMessage = ctx.message.text;
-    let newUserMessage = userMessage.split('');
-    let testBuff = [];
-    newUserMessage.forEach(element => {
-        if (element == ' '){
-            testBuff.push(' ')
-        }
-        test.forEach(key => {
-            if (element.toLowerCase() == key[0]){
+  const userMessage = ctx.message.text;
+  const newUserMessage = userMessage.split('');
 
-                testBuff.push(key[1]);
-            }
+  const messID = ctx.message.message_id;
+  const chatID = ctx.message.chat.id;
 
-        })
+  const testBuff = [];
 
-    })
-    ctx.telegram.sendMessage(ctx.message.chat.id, testBuff.join(''));
+  newUserMessage.forEach((element) => {
+    if (element === ' ') {
+      testBuff.push(' ');
+    }
+    test.forEach((key) => {
+      if (element.toLowerCase() === key[0]) {
+        testBuff.push(key[1]);
+      }
+    });
+  });
 
-})
-bot.launch() // запуск бота
+  // ctx.telegram.deleteMessage(chatID, messID);
+  ctx.telegram.sendMessage(chatID, testBuff.join(''));
+});
 
-
+bot.launch();
